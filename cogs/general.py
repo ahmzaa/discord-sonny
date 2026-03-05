c519c7539c401d949c871c8bfa1d938b7ad770de
ahmza <hamza@ahmza.com>
2026-03-05 18:09:48 +0000

    improved clear commands

--- cogs/general.py
+++ cogs/general.py
@@ -15,16 +15,34 @@ class General(commands.Cog):
     @app_commands.command(name="clear", description="Delete Messages")
     @app_commands.checks.has_permissions(manage_messages=True)
     async def clear(self, interaction: discord.Interaction, amount: int):
-        await interaction.channel.purge(limit=amount, bulk=True)
-        await interaction.response.send_message(
-            "Channel Purged of heresey !!", ephemeral=True
-        )
+        await interaction.response.defer(ephemeral=True)
+        if amount < 1:
+            await interaction.followup.send("Please specify a number greater than 0.")
+            return
+        try:
+            deleted = await interaction.channel.purge(limit=amount)
+            await interaction.followup.send(
+                f"Successfully purged {len(deleted)} messages of heresey!",
+                ephemeral=True,
+            )
+        except Exception as e:
+            await interaction.followup.send(f"An Error occurred: {e}", ephemeral=True)
 
     @clear.error
     async def clear_error(
         self, interaction: discord.Interaction, error: app_commands.AppCommandError
     ):
-        await interaction.response.send_message(f"Error: {error}", ephemeral=True)
+        if isinstance(error, app_commands.MissingPermissions):
+            if interaction.response.is_done():
+                await interaction.followup.send(
+                    "You lack permission to purge heresy!", ephemeral=True
+                )
+            else:
+                await interaction.response.send_message(
+                    "You lack permission to purge heresy!", ephemeral=True
+                )
+        else:
+            print(f"Clear Command Error: {error}")
 
 
 async def setup(bot):
